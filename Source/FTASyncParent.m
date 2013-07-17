31ba5ed58e8d9b5483dfb5b69d4fb11631506dde
Rei Kubonaga <kubonagarei@gmail.com>
2013-07-17 19:22:19 +0900

    define unarchivePhotoData in FTAParent

--- Source/FTASyncParent.m
+++ Source/FTASyncParent.m
@@ -89,6 +89,22 @@
   }
 }
 
+- (id) unarchivePhotoData:(NSString *) propertyName {
+  id unarchiveObject;
+  @try {
+    unarchiveObject = [NSKeyedUnarchiver unarchiveObjectWithData:(NSData *)[self valueForKey:propertyName]];
+  }@catch (NSException *exception) {
+    // if nsdata is uiimage, exception occurred.
+    unarchiveObject = nil;
+    NSLog(@"unarchive exception name  :%@",exception.name);
+    NSLog(@"unarchive exception reason:%@",exception.reason);
+  }
+  if ([[unarchiveObject class] isSubclassOfClass:[NSURL class]]) {
+    return unarchiveObject;
+  }
+  return [UIImage imageWithData: [self valueForKey:propertyName]];
+}
+
 #pragma mark - KVO
 
 - (void)setupRelationshipObservation {
